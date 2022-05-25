4112d81c8302a69e121e4ea9c0008fc4dfbc0558
Luca Agostini <luca@betcha.com>
2022-05-25 19:43:31 +0200

    fix: exporting the right module name

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
-const Braintree = NativeModules.Braintree
-  ? NativeModules.Braintree
+const Braintree = NativeModules.BrainTreeDropIn
+  ? NativeModules.BrainTreeDropIn
   : new Proxy(
     {},
     {

126e745d4bef6158a2ad010d287ece12713018e6
Luca Agostini <luca.agostini@vividseats.com>
2024-07-25 10:32:08 +0200

    upd: added venmo support for android

--- android/src/main/java/com/reactnativebraintree/BraintreeModule.kt
+++ android/src/main/java/com/reactnativebraintree/BraintreeModule.kt
@@ -1,26 +1,137 @@
 package com.reactnativebraintree
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.FragmentActivity
-import com.braintreepayments.api.*
+import com.braintreepayments.api.BraintreeClient
+import com.braintreepayments.api.BraintreeRequestCodes
+import com.braintreepayments.api.BrowserSwitchResult
+import com.braintreepayments.api.Card
+import com.braintreepayments.api.CardClient
+import com.braintreepayments.api.DataCollector
+import com.braintreepayments.api.DataCollectorRequest
+import com.braintreepayments.api.PayPalAccountNonce
+import com.braintreepayments.api.PayPalCheckoutRequest
+import com.braintreepayments.api.PayPalClient
+import com.braintreepayments.api.PayPalListener
+import com.braintreepayments.api.PayPalPaymentIntent
+import com.braintreepayments.api.PayPalVaultRequest
+import com.braintreepayments.api.UserCanceledException
+import com.braintreepayments.api.VenmoAccountNonce
+import com.braintreepayments.api.VenmoClient
+import com.braintreepayments.api.VenmoPaymentMethodUsage
+import com.braintreepayments.api.VenmoRequest
+import com.braintreepayments.api.VenmoTokenizeAccountCallback
+import com.facebook.react.bridge.ActivityEventListener
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.LifecycleEventListener
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.WritableMap
-import com.facebook.react.bridge.Arguments
-import java.lang.Exception
+
 
 class BraintreeModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext), PayPalListener {
+  ReactContextBaseJavaModule(reactContext), PayPalListener, ActivityEventListener, LifecycleEventListener {
 
   val appContext: Context = reactContext.applicationContext
+
   private var paypalPromise: Promise? = null
+  private var venmoPromise: Promise? = null
+  private var venmoClient: VenmoClient? = null
+  private var apiClient: BraintreeClient? = null
+
+  init {
+    reactContext.addLifecycleEventListener(this)
+    reactContext.addActivityEventListener(this)
+  }
 
 
   override fun getName(): String {
     return "BrainTreeDropIn"
   }
+
+
+
+
+  override fun onActivityResult(
+    activity: Activity?,
+    requestCode: Int,
+    resultCode: Int,
+    intent: Intent?
+  ) {
+    when (requestCode) {
+      BraintreeRequestCodes.VENMO -> venmoClient.let {
+        venmoClient?.onActivityResult(
+          appContext,
+          resultCode,
+          intent,
+          ::onVenmoResult
+        )
+      }
+    }
+  }
+
+
+  override fun onNewIntent(intent: Intent?) {
+    currentActivity.let {
+      currentActivity?.setIntent(intent)
+    }
+  }
+
+  override fun onHostResume() {
+    currentActivity.let {
+       val browserSwitchResult: BrowserSwitchResult? =
+         apiClient?.deliverBrowserSwitchResult(currentActivity as FragmentActivity)
+      if (browserSwitchResult != null) {
+        when (browserSwitchResult.requestCode) {
+
+          BraintreeRequestCodes.VENMO ->venmoClient.let {
+            venmoClient?.onBrowserSwitchResult(
+              browserSwitchResult,
+              ::onVenmoResult
+            )
+          }
+        }
+      }
+    }
+  }
+
+  private fun onVenmoResult (venmoAccountNonce: VenmoAccountNonce?, error: Exception?) {
+    venmoAccountNonce?.let {
+      try {
+        var data: WritableMap = Arguments.createMap()
+        data.putString("nonce", venmoAccountNonce.string)
+        data.putString("firstName", venmoAccountNonce.firstName)
+        data.putString("lastName", venmoAccountNonce.lastName)
+        data.putString("payerID", venmoAccountNonce.username)
+        data.putString("email", venmoAccountNonce.email)
+
+        venmoPromise?.resolve(data)
+      } catch (e: Error) {
+        venmoPromise?.reject("Fetch_venmo_token_error", "Error tokenizing venmo account", e)
+      }
+    } ?: error?.let {
+      if (error is UserCanceledException) {
+        venmoPromise?.reject("Venmo user canceled", "Venmo user canceled", null)
+      } else {
+        venmoPromise?.reject(error)
+      }
+
+    } ?: run {
+      venmoPromise?.reject("Fetch_venmo_token_error", "Error tokenizing venmo account")
+    }
+
+  }
+
+  override fun onHostPause() {
+  }
+
+  override fun onHostDestroy() {
+  }
+
 
 
   // Example method
@@ -193,6 +304,45 @@ class BraintreeModule(reactContext: ReactApplicationContext) :
   }
 
   @ReactMethod
+  fun startVenmoCheckout (clientToken: String, promise:Promise){
+    try {
+
+      apiClient = BraintreeClient(appContext, clientToken)
+      if (apiClient == null) {
+        throw Error("unable to instanciate apiClient")
+      }
+
+
+      val request = VenmoRequest(VenmoPaymentMethodUsage.MULTI_USE)
+      //request.profileId = "your-profile-id"
+      request.shouldVault = true
+      request.fallbackToWeb = true
+
+      venmoPromise = promise
+
+      currentActivity?.runOnUiThread {
+
+        venmoClient = VenmoClient( apiClient!!)
+
+        venmoClient?.tokenizeVenmoAccount((currentActivity as FragmentActivity), request,
+          VenmoTokenizeAccountCallback { error: java.lang.Exception? ->
+            error?.let{
+              promise.reject(error)
+            }
+          })
+
+
+
+      }
+
+    } catch (e: Error) {
+      promise.reject(e)
+    }
+
+  }
+
+
+  @ReactMethod
   fun collectDeviceData(clientToken: String,
                         sandbox:Boolean,
                         promise: Promise){
@@ -218,5 +368,6 @@ class BraintreeModule(reactContext: ReactApplicationContext) :
 
 
   }
+
 
 }
